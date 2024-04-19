a2385b4d46b323cadc8ff527ec13d814d03bdea5
Deepanshu <deepanshu.9dd@gmail.com>
2024-04-19 20:27:06 +0530

    feat: add homeContent page

---
+++ src/pages/HomeContent/HomeContent.tsx
@@ -0,0 +1,33 @@
+import notes from "../../assets/notes.png";
+import "./homeContent.css";
+import { Link, useNavigate } from "react-router-dom";
+
+export default function HomeContent() {
+  const navigate = useNavigate();
+
+  function handleClick(e) {
+    e.preventDefault();
+    navigate("/signUp");
+  }
+  return (
+    <>
+      <div className="main-content">
+        <div className="image-container">
+          <img src={notes} alt="" />
+        </div>
+        <div className="bottom-panel">
+          <p>
+            Create & design your <strong>Notes Easily</strong>
+          </p>
+          <button onClick={handleClick}>Get Started</button>
+          <span>
+            Already have an account?{" "}
+            <strong>
+              <Link to="/login">Log In</Link>
+            </strong>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
