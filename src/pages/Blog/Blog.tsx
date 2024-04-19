8580c897874b1ca6a61918f278bcfe76cf669a6d
Deepanshu <deepanshu.9dd@gmail.com>
2024-04-19 20:29:14 +0530

    feat: add blog page

---
+++ src/pages/Blog/Blog.tsx
@@ -0,0 +1,11 @@
+import "./blog.css";
+
+export default function Blog() {
+  return (
+    <>
+      <div className="blog-container">
+        This is Blog page and you can add your favourite blogs here
+      </div>
+    </>
+  );
+}
