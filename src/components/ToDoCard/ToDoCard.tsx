54783ae6f289f4ec6dc1774e86a25c76c53f9132
Deepanshu <deepanshu.9dd@gmail.com>
2024-04-19 20:38:34 +0530

    feat: add ToDoCard page

---
+++ src/components/ToDoCard/ToDoCard.tsx
@@ -0,0 +1,12 @@
+import "./toDoCard.css";
+
+export default function ToDoCard() {
+  return (
+    <>
+      <div className="todo-container">
+        <input type="checkbox" />
+        <p>Bath</p>
+      </div>
+    </>
+  );
+}
