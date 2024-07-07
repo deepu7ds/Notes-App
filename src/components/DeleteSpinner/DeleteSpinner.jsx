56ac201406142a3763deb41e60e43383f214686c
Deepanshu <deepanshu.9dd@gmail.com>
2024-07-07 20:25:17 +0530

    feat : add deleteSpinner

---
+++ src/components/DeleteSpinner/DeleteSpinner.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./deleteSpinner.css";
+
+const DeleteSpinner = () => (
+  <div className="deleting-overlay">
+    <div className="deleting-message">Deleting...</div>
+  </div>
+);
+export default DeleteSpinner;
